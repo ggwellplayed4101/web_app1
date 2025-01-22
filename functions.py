650d7898230feb7fe493774ad4a563c79b2fc45e
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-01-22 14:41:58 -0600

    changes

--- functions.py
+++ functions.py
@@ -1,15 +1,27 @@
 FILEPATH = "todos.txt"
 
+
+
+
 def get_todos(filepath=FILEPATH):
-    """ Read a text file and return the list of to-do items."""
-    with open(filepath, "r") as file_local:
-            todos_local = file_local.readlines()
-            return todos_local
+    """ Read a text file and return the list of
+    to-do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
 
-def write_todos(todos_local, filepath=FILEPATH):
-    """Writes the to-do items list in the text file."""
-    with open(filepath, "w") as file_local:
-        file_local.writelines(todos_local)
 
-if __name__  == "__main__":
-    print("hello.")
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    """ Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file:
+        file.writelines(todos_arg)
+
+
+
+
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
--- web.py
+++ web.py
@@ -1,21 +1,19 @@
 import streamlit as st
 import functions
-import os
-
-if not os.path.exists("todos.txt"):
-    with open("todos.txt", "w"):
-        pass
 
 todos = functions.get_todos()
+
 
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
     functions.write_todos(todos)
 
+
 st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is to increase my productivity")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -23,9 +21,8 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.rerun()
+        st.experimental_rerun()
 
-st.text_input(label=" ", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
 
-st.session_state
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key='new_todo')
